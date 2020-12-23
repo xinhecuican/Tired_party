ff5346a8219aed4d20cd6a2e2dfede2320f64cca
xinhecuican <lizilin@tju.edu.cn>
2020-12-23 22:27:33 +0800

    try

--- Tired_party/SubModule.cs
+++ Tired_party/SubModule.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus;
 using TaleWorlds.Core;
 using TaleWorlds.Engine;
 using TaleWorlds.Engine.Screens;
@@ -15,17 +16,44 @@ namespace Tired_party
 {
     public class SubModule : MBSubModuleBase
     {
+        private static Hourly_decrease_behaviour hourly_behaviour;
+
+        internal static Hourly_decrease_behaviour Hourly_behaviour { get => hourly_behaviour; set => hourly_behaviour = value; }
+
         /// <summary>
         /// 游戏处于加载界面时最先被调用的函数，你应该在这个函数中完成初始化的主要部分
         /// </summary>
         protected override void OnSubModuleLoad()
         {
+            base.OnSubModuleLoad();
             Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Message",
-                new TextObject("模板消息", null), 9990,
+                new TextObject("try", null), 9990,
                 () => { 
                     InformationManager.DisplayMessage(new InformationMessage("Hello World!"));
                 },
                 false));
+        }
+
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+            bool flag = (game.GameType is Campaign);
+            if(flag)
+            {
+                this.InitializeGame(game, gameStarterObject);
+            }
+                
+        }
+
+        private void InitializeGame(Game game, IGameStarter gameStarter)
+        {
+            SubModule.Hourly_behaviour = new Hourly_decrease_behaviour();
+            this.AddBehaviours(gameStarter as CampaignGameStarter);
+        }
+
+        private void AddBehaviours(CampaignGameStarter starter)
+        {
+            starter.AddBehavior(SubModule.hourly_behaviour);
         }
 
         protected override void OnApplicationTick(float dt)
@@ -34,18 +62,14 @@ namespace Tired_party
         }
         private void On_key_press()
         {
-            bool flag = Input.IsKeyDown(InputKey.G) ;
-            bool flag2 = Game.Current != null && Game.Current.GameStateManager != null 
-                && Game.Current.GameStateManager.ActiveState != null && Game.Current.GameStateManager.ActiveState.GetType() == typeof(MapState) 
+            bool flag = Input.IsKeyDown(InputKey.G);
+            bool flag2 = Game.Current != null && Game.Current.GameStateManager != null
+                && Game.Current.GameStateManager.ActiveState != null && Game.Current.GameStateManager.ActiveState.GetType() == typeof(MapState)
                 && !Game.Current.GameStateManager.ActiveState.IsMission && !Game.Current.GameStateManager.ActiveState.IsMenuState;
             bool flag3 = flag && flag2;
             if (flag3)
             {
-                
-                float hour = CampaignTime.Now.GetHourOfDay;
-                InformationManager.DisplayMessage(new InformationMessage(new TextObject("{=test_of_me_only}Time:"+hour.ToString(), null).ToString()
-                        , Color.FromUint(4282569842U)));
-                
+                GameMenu.SwitchToMenu("village");
             }
         }
     }
